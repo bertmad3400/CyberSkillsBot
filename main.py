a66c8396b325e93a05fb8f3600020acd7311e645
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-24 11:38:26 +0100

    Add skeleton for discord bot

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import random
+from pathlib import Path
+
+token = Path("./token.secret").read_text()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as "{client.user}"')
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith("!cs") and message.authorr != client.user:
+        messageContents = message.content.lower()
+        return
+
+    else:
+        return
+
+
+client.run(token)

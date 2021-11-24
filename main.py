fc5cd576752b3fb2f707adc2c5df75244aaecdaf
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-24 11:58:47 +0100

    Add function for querying cyberskills api

--- main.py
+++ main.py
@@ -3,10 +3,23 @@
 import discord
 import random
 from pathlib import Path
+import requests
+import json
 
 token = Path("./token.secret").read_text()
 
 client = discord.Client()
+
+async def getNextEvents(command):
+    numberOfEvents = command.split(" ")[-1]
+
+    if not numberOfEvents.isnumeric():
+        numberOfEvents = 3
+
+    return requests.get(f"https://bertmad.dk/api/cyberskills/nextEvents/{str(numberOfEvents)}/").json()
+
+async def getEventsOfThisMonth():
+    return requests.get("https://bertmad.dk/api/cyberskills/currentEvents/")
 
 @client.event
 async def on_ready():
